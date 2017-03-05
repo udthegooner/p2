b4a39d95f4bbfea00afcdf086751821232c48016
mperry3 <mperry3@wisc.edu>
2017-03-05 13:01:41 -0600

    Add files via upload

--- Scoreboard.java
+++ Scoreboard.java
@@ -6,36 +6,39 @@
 // TEAM:    46 Paras
 // Author1: (Daniel Jones, djones39@wisc.edu, lecture 2)
 /**
- * The Scoreboard class holds a ListADT<Job> of all the completed jobs.
- *  This class allows one to get the total score earned so far, update the score board,
- *  and thus score, and also print out a display of the Score board.
+ * The Scoreboard class holds a ListADT<Job> of all the completed jobs. This
+ * class allows one to get the total score earned so far, update the score
+ * board, and thus score, and also print out a display of the Score board.
  *
- * <p>Bugs: None known
+ * <p>
+ * Bugs: None known
  *
- * @author Daniel Jones
+ * @author Daniel Jones Matthew Perry
  */
-public class Scoreboard implements ScoreboardADT{
-	ListADT<Job> list = new JobList(); //TODO Parameters for the JobList
-	int total = 0;
-	
+public class Scoreboard implements ScoreboardADT {
+
+	// Creates a new JobList to store jobs.
+	JobList<Job> list = new JobList<Job>(null);
+
 	/**
-	 * This method allows one to add a Job object to list after the job was completed.
+	 * This method allows one to add a Job object to list after the job was
+	 * completed.
 	 *
 	 * PRECONDITIONS: A job was completed
 	 * 
 	 * POSTCONDITIONS: The list has one new job at the end
 	 *
-	 * @param job A Job object to be added to list
+	 * @param job
+	 *            A Job object to be added to list
 	 * @return void
 	 */
-	public void updateScoreBoard(Job job){
+	public void updateScoreBoard(Job job) {
 		list.add(job);
-		total += job.getPoints();
 	}
-	
+
 	/**
-	 * This method returns the total score earned by the player by adding the score int of every
-	 * job in list.
+	 * This method returns the total score earned by the player by adding the
+	 * score int of every job in list.
 	 *
 	 * PRECONDITIONS: none
 	 * 
@@ -43,12 +46,17 @@ public class Scoreboard implements ScoreboardADT{
 	 *
 	 * @return The int total, which is the number of earned points.
 	 */
-	public int getTotalScore(){
+	public int getTotalScore() {
+		int total = 0;
+		for (int i = 0; i < list.size(); ++i) {
+			total = total + list.get(i).getPoints();
+		}
 		return total;
 	}
-	
+
 	/**
-	 * This method allows one to print information about all completed jobs in a premade format.
+	 * This method allows one to print information about all completed jobs in a
+	 * premade format.
 	 *
 	 * PRECONDITIONS: none
 	 * 
@@ -56,9 +64,8 @@ public class Scoreboard implements ScoreboardADT{
 	 *
 	 * @return void
 	 */
-	public void displayScoreBoard(){
-		System.out.println("Total Score: " + getTotalScore() + "\nThe jobs completed:");
-		for(int i = 0; i < list.size(); ++i){
+	public void displayScoreBoard() {
+		for (int i = 0; i < list.size(); ++i) {
 			System.out.println("Job Name: " + list.get(i).getJobName());
 			System.out.println("Points earned for this job: " + list.get(i).getPoints());
 			System.out.println("--------------------------------------------");
